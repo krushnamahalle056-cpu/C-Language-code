0b32aa2bea2ac8ae11b7b364935c5b12194b0da0
Krushna mahalle <krushnamahalle056@gmail.com>
2026-06-21 23:20:59 +0530

    new code add

--- Quick_Sort.c
+++ Quick_Sort.c
@@ -16,3 +16,12 @@ int partition(int arr[], int low, int high){
     arr[high] = temp;
     return i + 1;
 }
+
+void quickSort(int arr[], int low, int high){
+    if(low < high){
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
