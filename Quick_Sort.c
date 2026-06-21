f4f1c5f1598df6aeb4c1ed1e2ab9c7b649892e0d
Krushna mahalle <krushnamahalle056@gmail.com>
2026-06-21 23:32:24 +0530

    add canges

--- Quick_Sort.c
+++ Quick_Sort.c
@@ -25,3 +25,13 @@ void quickSort(int arr[], int low, int high){
     }
 }
 
+int main(){
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quickSort(arr, 0, n - 1);
+    printf("Sorted array: \n");
+    for(int i = 0; i < n; i++){
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
