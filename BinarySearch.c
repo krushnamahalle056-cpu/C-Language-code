6b3b336ce1a2b3f07c89e499ee5adbb1e47a7ee0
Krushna mahalle <krushnamahalle056@gmail.com>
2026-06-22 20:22:51 +0530

    add some code

--- BinarySearch.c
+++ BinarySearch.c
@@ -1,1 +1,22 @@
 #include<stdio.h>
+// first of all for binary search the array must be sorted
+int binarySearch(int arr[], int size, int key){
+    int start = 0;
+    int end = size - 1;
+
+    while(start <= end){
+        int mid = (start + end) / 2;
+
+        if(arr[mid] == key){
+         return arr[mid];}
+        }
+        else if(arr[mid] < key){
+            start = mid + 1;
+        }
+        else{
+            end = mid - 1;
+        }
+    }
+    return -1; // Key not found
+}
+
